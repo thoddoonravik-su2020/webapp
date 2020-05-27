e4269bbe4498f8ba7927e08d70b375149c398708
root <thoddoonravi.k@northeastern.edu>
2020-05-27 08:56:16 -0400

    user details and password integrity added

---
+++ webapp/core/user.js
@@ -0,0 +1,54 @@
+const connection = require('./dbconnection');
+const bcrypt = require('bcrypt');
+
+//function for the user setup
+function user() {};
+
+user.prototype = {
+    //checks user is not an integer 
+    find : function(user = null, callback){
+        if(user){
+            var field = Number.isInteger(user) ?'id':'username';
+
+        }
+
+        //if the user is string then returns input_fields to enter values
+        let sqlQuery = `SELECT * FROM users WHERE ${field} =?`;
+        connection.query(sqlQuery,user,function(err,result){
+            if(err) throw console.error('error');
+            callback(result);
+            
+        });
+    },
+    create: function(body, callback )
+    {
+        let passwd = body.password;
+        body.password = bcrypt.hashSync(passwd,10);
+
+        var usrArray = [];
+        for(prop in body){
+            usrArray.push(prop);
+        }
+
+        let sqlQuery = `INSERT INTO users (firstname,lastname,password,email) VALUES(?,?,?)`;
+
+        connection.query(sqlQuery, usrArray, function(err, lastname){
+            if(err) throw console.error('error 2');
+            callback(lastname);
+            
+        });
+
+    },
+    login : function(lastname,password,callback){
+        this.find(lastname, function(user){
+            if(user){
+                if(bcrypt.compareSync(password, user.password)){
+                    callback(user);
+                    return;
+                }
+            }
+            callback(null);
+        });
+    }
+}
+module.exports = user;
