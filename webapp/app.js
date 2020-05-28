dff6131cf3b91c2ef62936dfa4d185359f811a9f
root <thoddoonravi.k@northeastern.edu>
2020-05-28 13:15:24 -0400

    user login and registraion complete with secure pwd

--- webapp/app.js
+++ webapp/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const pageRouter = require('./routes/route');
+const router = require('./routes/route');
 //const expressLayouts = require('express-ejs-layouts');
 
 //body parser
 app.use(express.urlencoded({extended:false}));
+
+app.use('/', router);
 
 //static assests
 app.use(express.static(path.join(__dirname,'assets')));
@@ -14,40 +16,18 @@ app.use(express.static(path.join(__dirname,'assets')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// //for redirecting errors
-// app.use((req, res, next)=>{
-//     const err = new Error('Page cannot be found !!');
-//     err.status =404;
-//     next(err);
-// })
-
-// //to handle errors
-// app.use((req,res)=> {
-//     res.status(err.status || 500);
-//     res.send(err.message);
-// })
-
-//for route
-app.use('/', pageRouter);
-
-//for home page
-app.get('/', (req,res)=>{
-    res.render('index');
+//for redirecting errors
+app.use((req, res, next)=>{
+    const err = new Error('Page cannot be found !!');
+    err.status =404;
+    next(err);
 })
 
-//for login page
-app.get('/login', (req,res)=>{
-    res.render('/')
+//to handle errors
+app.use((req,res)=> {
+    res.status(err.status || 500);
+    res.send(err.message);
 })
-
-//for register page
-app.get('/register', (req,res)=>{
-    res.render('/');
-})
-
-
-
-
 
 const PORT = process.env.PORT || 3000;
 //port setting for server
