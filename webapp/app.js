1c0c45fb589e0494d3db18797bfc7356827587e6
root <thoddoonravi.k@northeastern.edu>
2020-05-30 05:10:12 -0400

    installed layouts required

--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,8 @@
 const express = require ('express');
+const ex
 const app = express();
 //for route
 app.use('/', require('./routes/route'));
+app.use('/user', require('./routes/user'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server listening on ${PORT}`));
