2e23a731c520d65b48caaa538ad82237f1c6c85e
root <thoddoonravi.k@northeastern.edu>
2020-05-30 07:43:32 -0400

    registration layout with booswatch and fontawesome

--- webapp/routes/user.js
+++ webapp/routes/user.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const router = express.Router();
 //Login Page
-router.get('/login', (request, response)=> response.send('Login'));
+router.get('/login', (request, response)=> response.render('loginpage'));
 //Register Page
-router.get('/register', (request, response)=> response.send('Register'));
+router.get('/register', (request, response)=> response.render('registerpage'));
 module.exports=router;
