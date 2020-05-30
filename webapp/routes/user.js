bf0a719db4b1b7e711e5670038a4bad9a129dc0e
root <thoddoonravi.k@northeastern.edu>
2020-05-30 10:44:47 -0400

    password validations

--- webapp/routes/user.js
+++ webapp/routes/user.js
@@ -4,4 +4,28 @@ const router = express.Router();
 router.get('/login', (request, response)=> response.render('loginpage'));
 //Register Page
 router.get('/register', (request, response)=> response.render('registerpage'));
+//Post Register
+router.post('/register', (req, res)=>{
+   const {firstname, lastname, email, password, password2} = req.body;
+   let errors =[];
+   //Password mismatch
+   if(password !== password2){
+       errors.push({msg: 'Entered passwords do not match !!'})
+   }
+
+   if(firstname == lastname){
+       errors.push({msg: 'Please enter a valid firstname and lastname'})
+   }
+   if(password.length < 8){
+       errors.push({msg: 'passwords cannot be less than 8 characters'})
+   }
+   if(errors < 0){
+       res.send('pass');
+   }
+   else
+   {
+       res.render('registerpage')
+   }
+});
+
 module.exports=router;
