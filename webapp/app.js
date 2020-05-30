3822b8028841c33e77e1c75ae79cb2b0ee9446c9
root <thoddoonravi.k@northeastern.edu>
2020-05-30 04:58:34 -0400

    a3 test

--- webapp/app.js
+++ webapp/app.js
@@ -1,38 +1,6 @@
-const express = require('express');
-const path = require('path');
+const express = require ('express');
 const app = express();
-const router = require('./routes/route');
-//const expressLayouts = require('express-ejs-layouts');
-
-//body parser
-app.use(express.urlencoded({extended:false}));
-
-app.use('/', router);
-
-//static assests
-app.use(express.static(path.join(__dirname,'assets')));
-
-//views
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-//for redirecting errors
-app.use((req, res, next)=>{
-    const err = new Error('Page cannot be found !!');
-    err.status =404;
-    next(err);
-})
-
-//to handle errors
-app.use((req,res)=> {
-    res.status(err.status || 500);
-    res.send(err.message);
-})
-
+//for route
+app.use('/', require('./routes/route'));
 const PORT = process.env.PORT || 3000;
-//port setting for server
-app.listen(3000, ()=> {
-    console.log(`Server is listening on port ${PORT}`);
-});
-
-module.exports = app;
+app.listen(PORT, console.log(`Server listening on ${PORT}`));
