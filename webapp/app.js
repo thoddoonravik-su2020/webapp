6a1307a8ca26a860c326501f7f308bad36785e20
root <thoddoonravi.k@northeastern.edu>
2020-05-31 14:19:02 -0400

    m

--- webapp/app.js
+++ webapp/app.js
@@ -1,38 +1,21 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const router = require('./routes/route');
-//const expressLayouts = require('express-ejs-layouts');
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var app = express()
+var port = process.env.PORT || 3000
 
-//body parser
-app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
 
-app.use('/', router);
+var Users = require('./routes/user')
 
-//static assests
-app.use(express.static(path.join(__dirname,'assets')));
+app.use('/users', Users)
 
-//views
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-//for redirecting errors
-app.use((req, res, next)=>{
-    const err = new Error('Page cannot be found !!');
-    err.status =404;
-    next(err);
+app.listen(port, function() {
+  console.log('Server is running on port: ' + port)
 })
-
-//to handle errors
-app.use((req,res)=> {
-    res.status(err.status || 500);
-    res.send(err.message);
-})
-
-const PORT = process.env.PORT || 3000;
-//port setting for server
-app.listen(3000, ()=> {
-    console.log(`Server is listening on port ${PORT}`);
-});
-
-module.exports = app;
