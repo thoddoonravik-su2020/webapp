6818ac2c890329fc35f43e8c64b0ddf718bbf50a
root <thoddoonravi.k@northeastern.edu>
2020-06-28 17:34:47 -0400

    a

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+cd /home/ubuntu/webapp/webapp
+sudo npm install
 cd /home/ubuntu/webapp/webapp/webapp/frontend/
 sudo ng build
 cd dist/frontend/
 sudo cp * /var/www/html/
 ls -al
-cd /home/ubuntu/webapp/webapp
-sudo npm install
 cd /home/ubuntu/webapp/webapp/webapp
 node app.js > /dev/null 2> /dev/null < /dev/null &
 
