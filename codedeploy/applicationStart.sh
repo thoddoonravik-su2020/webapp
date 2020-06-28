cec245f79372c232e561f565d505e20d16192947
root <thoddoonravi.k@northeastern.edu>
2020-06-28 16:56:07 -0400

    .

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+mv /home/ubuntu/webapp-UI/* /var/www/html/
 ls -al
 cd /home/ubuntu/webapp/webapp
 npm install
 node app.js > /dev/null 2> /dev/null < /dev/null &
+
+
--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 sudo rm -rf /home/ubuntu/webapp
+
+sudo rm -rf /var/www/html/*
+export app_root=/home/ubuntu/webapp-UI
+if [ -d "$app_root" ];then
+    rm -rf /home/ubuntu/webapp-UI
+    mkdir -p /home/ubuntu/webapp-UI
+else
+    mkdir -p /home/ubuntu/webapp-UI
+fi
