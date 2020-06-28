64efe6133ef9118af10b529dfa994b19fc0ecc95
root <thoddoonravi.k@northeastern.edu>
2020-06-28 13:21:35 -0400

    bug fix

--- codedeploy/AfterInstall.sh
+++ codedeploy/AfterInstall.sh
@@ -2,8 +2,3 @@
 ls -al
 cd /home/ubuntu/webapp/frontend
 npm run ng build
-sudo mv Archive.zip /var/www/html
-cd /var/www/html/
-sudo rm -rf index.html
-sudo unzip Archive.zip
-sudo rm -rf Archive.zip
