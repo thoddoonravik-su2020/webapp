19b041e743e11168280366ee29cf78eb32552588
root <thoddoonravi.k@northeastern.edu>
2020-06-28 18:24:57 -0400

    bug fix

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 cd /home/ubuntu/webapp/webapp/webapp/frontend/
 sudo npm install
 sudo ng build
 cd dist/frontend/
 sudo cp * /var/www/html/
-cd
-cd /home/ubuntu/webapp/webapp/webapp
-node app.js > /dev/null 2> /dev/null < /dev/null &
+
 
