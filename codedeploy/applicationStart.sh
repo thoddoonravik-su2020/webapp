3a3f8dd4ce79b1644a85a7955872a6f6f1b064eb
Keerthana Thoddoon Ravi <55114981+thoddoonravik@users.noreply.github.com>
2020-06-28 15:43:05 -0400

    A6 (#51)
    * test commit
    
    * test commit
    
    * test commit..
    
    * assignment branch test
    
    * changes saved in a2
    
    * test
    
    * inital commit
    
    * vscode to gitignore
    
    * necessary changes
    
    * folder structure
    
    * server 3000 is listening
    
    * rendering views
    
    * routing files created
    
    * database created and connection successfull
    
    * user details and password integrity added
    
    * installing pug
    
    * change in front end layout
    
    * applying styles
    
    * database issue parser error fixed
    
    * userdetails added
    
    * user login and registraion complete with secure pwd
    
    * Update README
    
    * Update README
    
    * Delete config.yml
    
    * config.yml
    
    * resolve conflicts
    
    * Update registrationForm.ejs
    
    * test
    
    * test
    
    * a3 test
    
    * installed layouts required
    
    * resolving frontend issues
    
    * changing pug to ejs
    
    * using bootswatch open source for layouts
    
    * using js script bootstrap
    
    * views updated
    
    * registration layout with booswatch and fontawesome
    
    * styles and requred libraries updated
    
    * dependency required
    
    * database connection established
    
    * password validations
    
    * alert created
    
    * database used sequelized and success
    
    * login issues of previous assignment fixed
    
    * save changes
    
    * changing frontend
    
    * previous assignment issues fixed and added angular to a3
    
    * commit registration issues
    
    * app
    
    * computing
    
    * m
    
    * Book Details Front End Done
    
    * seller module complete
    
    * adding circleci
    
    * test config removed
    
    * update functionility of books addded
    
    * vscode git ignore
    
    * .idea gitignore
    
    * .swp
    
    * change in READMD instruction
    
    * remove .vscode
    
    * rm swp
    
    * book component
    
    * Delete .app.js.swp
    
    * Delete launch.json
    
    * UI issues fixed
    
    * fixed layouts
    
    * folder structure changes
    
    * webapp
    
    * git issue
    
    * issues fixed
    
    * restructured
    
    * structure
    
    * resolve conflicts
    
    * circle ci moved
    
    * adding single image delete functionality
    
    * change in README
    
    * test
    
    * env
    
    * s3 bucket
    
    * yml changes for deploy
    
    * s3 bucket error fixed
    
    * s3
    
    * fixing issues
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * artifact
    
    * a
    
    * a
    
    * a
    
    * a
    
    * appspec
    
    * merge
    
    * a
    
    * a
    
    * a
    
    * error fixed
    
    * installation of node
    
    * push
    
    * frontend
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * .
    
    * .
    
    * a
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 sudo rm -rf /home/ubuntu/webapp
 node server.js > /dev/null 2> /dev/null < /dev/null &
-cd /home/ubuntu/webapp/webapp/frontend
-npm run ng build
-cd /dist/frontend
-mv /dist/frontend/* /var/www/html/
+mkdir hello
 
--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -3,7 +3,6 @@ ls -al
 cd /home/ubuntu/webapp
 ls -al
 npm install
-mkdir hello
 # cd /home/ubuntu/webapp/webapp/frontend
 # npm run ng build
 # cd /dist/frontend
