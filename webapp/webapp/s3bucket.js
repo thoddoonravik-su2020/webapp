82395423fa4870551d4039eae7313963b52fbeb4
root <thoddoonravi.k@northeastern.edu>
2020-07-03 09:10:44 -0400

    curl command test

--- webapp/webapp/s3bucket.js
+++ webapp/webapp/s3bucket.js
@@ -3,10 +3,10 @@ const AWS = require('aws-sdk');
 const ID = process.env.accessKeyId;
 const SECRET = process.env.secretAccessKey;
 
-const s3 = new AWS.S3({
+// const s3 = new AWS.S3({
     
-    accessKeyId: ID,
-    secretAccessKey: SECRET
-});
-
+//     accessKeyId: ID,
+//     secretAccessKey: SECRET
+// });
+const s3 = new AWS.S3();
 module.exports = s3;
