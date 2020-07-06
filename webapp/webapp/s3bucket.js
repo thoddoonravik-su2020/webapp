ded8c17f3b4b3028024339fbb99d163817577735
root <thoddoonravi.k@northeastern.edu>
2020-07-06 14:35:55 -0400

    env

--- webapp/webapp/s3bucket.js
+++ webapp/webapp/s3bucket.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 
-const ID = 'AKIAIX45E43NMFL2IWNA';
-const SECRET = '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';
+const ID = process.env.accessKeyId;
+const SECRET = process.env.secretAccessKey;
 
 const s3 = new AWS.S3({
     
