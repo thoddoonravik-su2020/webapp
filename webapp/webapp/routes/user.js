b1045021670810ad0e79bea96fb954fb0d9e206c
Keerthana Thoddoon Ravi <55114981+thoddoonravik@users.noreply.github.com>
2020-07-06 14:25:58 -0400

    A7 (#81)
    * test commit
    
    * test commit
    
    * test commit..
    
    * assignment branch test
    
    * changes saved in a2
    
    * test
    
    * inital commit
    
    * vscode to gitignore
    
    * necessary changes
    
    * folder structure
    
    * server 3000 is listening
    
    * rendering views
    
    * routing files created
    
    * database created and connection successfull
    
    * user details and password integrity added
    
    * installing pug
    
    * change in front end layout
    
    * applying styles
    
    * database issue parser error fixed
    
    * userdetails added
    
    * user login and registraion complete with secure pwd
    
    * Update README
    
    * Update README
    
    * Delete config.yml
    
    * config.yml
    
    * resolve conflicts
    
    * Update registrationForm.ejs
    
    * test
    
    * test
    
    * a3 test
    
    * installed layouts required
    
    * resolving frontend issues
    
    * changing pug to ejs
    
    * using bootswatch open source for layouts
    
    * using js script bootstrap
    
    * views updated
    
    * registration layout with booswatch and fontawesome
    
    * styles and requred libraries updated
    
    * dependency required
    
    * database connection established
    
    * password validations
    
    * alert created
    
    * database used sequelized and success
    
    * login issues of previous assignment fixed
    
    * save changes
    
    * changing frontend
    
    * previous assignment issues fixed and added angular to a3
    
    * commit registration issues
    
    * app
    
    * computing
    
    * m
    
    * Book Details Front End Done
    
    * seller module complete
    
    * adding circleci
    
    * test config removed
    
    * update functionility of books addded
    
    * vscode git ignore
    
    * .idea gitignore
    
    * .swp
    
    * change in READMD instruction
    
    * remove .vscode
    
    * rm swp
    
    * book component
    
    * Delete .app.js.swp
    
    * Delete launch.json
    
    * UI issues fixed
    
    * fixed layouts
    
    * folder structure changes
    
    * webapp
    
    * git issue
    
    * issues fixed
    
    * restructured
    
    * structure
    
    * resolve conflicts
    
    * circle ci moved
    
    * adding single image delete functionality
    
    * change in README
    
    * test
    
    * env
    
    * s3 bucket
    
    * yml changes for deploy
    
    * s3 bucket error fixed
    
    * s3
    
    * fixing issues
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * artifact
    
    * a
    
    * a
    
    * a
    
    * a
    
    * appspec
    
    * merge
    
    * a
    
    * a
    
    * a
    
    * error fixed
    
    * installation of node
    
    * push
    
    * frontend
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * .
    
    * .
    
    * a
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * a
    
    * a
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * issue
    
    * issue
    
    * added server
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * merge
    
    * server
    
    * artifacts
    
    * artifacts
    
    * a
    
    * app stop
    
    * curl command test
    
    * a7 changes
    
    * a7
    
    * s3 bug fix

--- webapp/webapp/routes/user.js
+++ webapp/webapp/routes/user.js
@@ -14,43 +14,43 @@ process.env.SECRET_KEY = 'secret'
 
 
 
-const AWS_ACCESS_KEY='AKIAIGDF6D3Z32EAFIVA';
-const AWS_SECRET_KEY='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';
-//GET IMAGE from s3
-// Our default route
-users.get('/image',(req,res)=>{
-  console.log('hello')
-  AWS.config.update({
-      accessKeyId: 'AKIAIGDF6D3Z32EAFIVA',
-      secretAccessKey: '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'
-    });
-let s3 = new AWS.S3();
-async function getImage(){
-  const params =  s3.getObject(
-    {
-        Bucket: 'webapp.keerthana.ravi',
-        Key: 'myImage1'
-      }
+// const AWS_ACCESS_KEY='AKIAIX45E43NMFL2IWNA';
+// const AWS_SECRET_KEY='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';
+// //GET IMAGE from s3
+// // Our default route
+// users.get('/image',(req,res)=>{
+//   console.log('hello')
+//   AWS.config.update({
+//       accessKeyId: 'AKIAIX45E43NMFL2IWNA',
+//       secretAccessKey: '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'
+//     });
+// let s3 = new AWS.S3();
+// async function getImage(){
+//   const params =  s3.getObject(
+//     {
+//         Bucket: 'webapp.keerthana.tr',
+//         Key: 'myImage1'
+//       }
     
-  ).promise();
-  return params;
-} 
-getImage()
-.then((img)=>{
-let image="<img src='data:image/jpeg;base64," + encode(img.Body) + "'" + "/>";
-let startHTML="<html><body></body>";
-let endHTML="</body></html>";
-let html=startHTML + image + endHTML;
-res.send(html)
-}).catch((e)=>{
-      res.send(e)
-})
-function encode(data){
-  let buf = Buffer.from(data);
-  let base64 = buf.toString('base64');
-  return base64
-  }
-  })
+//   ).promise();
+//   return params;
+// } 
+// getImage()
+// .then((img)=>{
+// let image="<img src='data:image/jpeg;base64," + encode(img.Body) + "'" + "/>";
+// let startHTML="<html><body></body>";
+// let endHTML="</body></html>";
+// let html=startHTML + image + endHTML;
+// res.send(html)
+// }).catch((e)=>{
+//       res.send(e)
+// })
+// function encode(data){
+//   let buf = Buffer.from(data);
+//   let base64 = buf.toString('base64');
+//   return base64
+//   }
+//   })
   
 
 //POST
--- webapp/webapp/s3bucket.js
+++ webapp/webapp/s3bucket.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 
-const ID = process.env.accessKeyId;
-const SECRET = process.env.secretAccessKey;
+const ID = 'AKIAIX45E43NMFL2IWNA';
+const SECRET = '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';
 
 const s3 = new AWS.S3({
     
@@ -9,4 +9,6 @@ const s3 = new AWS.S3({
     secretAccessKey: SECRET
 });
 
+
+// const s3 = new AWS.S3();
 module.exports = s3;
