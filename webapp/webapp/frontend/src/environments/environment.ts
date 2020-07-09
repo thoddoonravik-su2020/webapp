6f8552115262d3580721af1a9027b9a873a04567
Keerthana Thoddoon Ravi <55114981+thoddoonravik@users.noreply.github.com>
2020-07-09 12:05:52 -0400

    A7 (#122)
    * webapp
    
    * git issue
    
    * issues fixed
    
    * restructured
    
    * structure
    
    * resolve conflicts
    
    * circle ci moved
    
    * adding single image delete functionality
    
    * change in README
    
    * test
    
    * env
    
    * s3 bucket
    
    * yml changes for deploy
    
    * s3 bucket error fixed
    
    * s3
    
    * fixing issues
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * issue
    
    * artifact
    
    * a
    
    * a
    
    * a
    
    * a
    
    * appspec
    
    * merge
    
    * a
    
    * a
    
    * a
    
    * error fixed
    
    * installation of node
    
    * push
    
    * frontend
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * .
    
    * .
    
    * a
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * a
    
    * a
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * issue
    
    * issue
    
    * added server
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * merge
    
    * server
    
    * artifacts
    
    * artifacts
    
    * a
    
    * app stop
    
    * curl command test
    
    * a7 changes
    
    * a7
    
    * s3 bug fix
    
    * env
    
    * app spec
    
    * appspec
    
    * a
    
    * database
    
    * UI changes
    
    * server db
    
    * env values
    
    * application structure
    
    * env
    
    * logger
    
    * bug fix
    
    * interval change
    
    * path change
    
    * statsd usage
    
    * bug fix
    
    * folder structure
    
    * file path
    
    * file path
    
    * check
    
    * app
    
    * permission
    
    * stats
    
    * a
    
    * stats
    
    * kill
    
    * appstop
    
    * a
    
    * config
    
    * a
    
    * a
    
    * a
    
    * a
    
    * count
    
    * logger
    
    * a
    
    * l
    
    * sudo
    
    * db
    
    * db
    
    * books
    
    * systemd
    
    * file path
    
    * systemmd service
    
    * angular changes in ip
    
    * typo fix

--- webapp/webapp/frontend/src/environments/environment.ts
+++ webapp/webapp/frontend/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  "api_url": "http://" + window.location.origin.split('//')[1].split(':')[0] + ":3000/",
+
+  api_url: "http://" + window.location.origin.split('//')[1].split(':')[0] + ":3000/",
   enableDebug: false
 };
 
