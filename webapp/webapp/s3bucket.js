d6fc7734bf6d864e74dd06f44e784c5ec8a8026f
root <thoddoonravi.k@northeastern.edu>
2020-07-15 18:55:39 -0400

    changes

--- webapp/webapp/s3bucket.js
+++ webapp/webapp/s3bucket.js
@@ -3,6 +3,9 @@ const AWS = require('aws-sdk');
 const ID = process.env.accessKeyId;
 const SECRET = process.env.secretAccessKey;
 
+
+
+
 const s3 = new AWS.S3({
     
     accessKeyId: ID,
@@ -10,9 +13,4 @@ const s3 = new AWS.S3({
 });
 
 
-<<<<<<< HEAD
-// const s3 = new AWS.S3();
-//bucket
-=======
->>>>>>> bb8f1f376d760811d68d39bb19ac269202406bc1
 module.exports = s3;
